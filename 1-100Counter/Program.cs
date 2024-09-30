171285a824e33381144fa9c3e7451a20c001f4f1
davit <davit@GOSH>
2024-09-30 12:16:05 +0400

    Add project files.

---
+++ 1-100Counter/Program.cs
@@ -0,0 +1,29 @@
+﻿
+        Console.WriteLine("Choos counting system");
+        Console.WriteLine("1 - Count 1-100");
+        Console.WriteLine("2 - Only Even numbers to 50");
+       
+     
+int Choice = int.Parse(Console.ReadLine());
+if ( Choice == 1 )
+{ 
+    for (int Number = 1; Number <= 100; Number++)
+    {
+
+        Console.Write(Number + " ");
+    }
+    Console.WriteLine();
+}
+else if ( Choice == 2 )
+{      int Even = 2;
+    while (Even <= 50)
+    {
+        if (Even % 2 == 0)
+        {
+            Console.Write(Even + " ");
+        }
+        Even++;
+    }
+    }
+        Console.ReadLine();
+    
